6b46ea59014c634f7c9b36132f2e4f6ca1b65b6a
Mihai Ratiu <mihai.g.ratiu@gmail.com>
2015-06-26 21:42:45 +0300

    Small tweaks for test commit

--- src/main/java/com/bb/boot/web/WebController.java
+++ src/main/java/com/bb/boot/web/WebController.java
@@ -1,9 +1,11 @@
 package com.bb.boot.web;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Service
 public class WebController {
     @RequestMapping("/")
     String home() {
